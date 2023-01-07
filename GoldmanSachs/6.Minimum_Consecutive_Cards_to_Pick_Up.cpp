218f20b1dbbf8278150fb3dbd57e038255c9ab7d
Vipul Verma <vipulverma802@gmail.com>
2023-01-07 19:40:41 +0530

    Create 6.Minimum_Consecutive_Cards_to_Pick_Up.cpp

---
+++ GoldmanSachs/6.Minimum_Consecutive_Cards_to_Pick_Up.cpp
@@ -0,0 +1,19 @@
+/*
+link: https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/
+*/
+
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        int n = cards.size();
+        unordered_map<int,int> mp;
+        int res = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(mp.find(cards[i])!=mp.end()) {
+                res = min(res,i-mp[cards[i]]+1);
+            }
+            mp[cards[i]] = i;
+        }
+        return res==INT_MAX ? -1 : res;
+    }
+};
